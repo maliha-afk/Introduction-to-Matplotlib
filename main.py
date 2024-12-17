9ca7fef9bedde424468b4a1796efac10618487b3
maliha-afk <24amaliha@gmail.com>
2024-12-17 21:59:30 +0600

    main.py

--- main.py
+++ main.py
@@ -2,18 +2,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints=np.array([1,10])
-ypoints=np.array([5,8])
 
-#plt.plot(xpoints,ypoints)
-
-#plt.show()
-
-xpoints2=np.array([1,5,6,10])
-ypoints2=np.array([2,4,6,8])
-
-#plt.plot(xpoints2,ypoints2,"o",mec="r",ms=10)
-#plt.show()
 
 name=np.array(["atiya","ryan","zaheen","tahmid"])
 marks=np.array([70,80,65,90])
